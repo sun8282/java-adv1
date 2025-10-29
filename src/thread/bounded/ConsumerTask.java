0f2a1c0481731f8a518f75b51c47889b01198171
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-10-30 08:39:29 +0900

    add: ConsumerTask

---
+++ src/thread/bounded/ConsumerTask.java
@@ -0,0 +1,15 @@
+package thread.bounded;
+
+public class ConsumerTask implements Runnable {
+
+    private BoundedQueue queue;
+
+    public ConsumerTask(BoundedQueue queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        String data = queue.take();
+    }
+}
