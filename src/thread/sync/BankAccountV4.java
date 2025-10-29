2ed0c0c2df8198d89d137fce3bc907560c72bae4
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-10-29 09:38:27 +0900

    add: LockSupportMainV4

---
+++ src/thread/sync/BankAccountV4.java
@@ -0,0 +1,41 @@
+package thread.sync;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankAccountV4 implements BankAccount {
+
+    private int balance;
+
+    public BankAccountV4(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    private final Lock lock = new ReentrantLock();
+
+    @Override
+    public boolean withdraw(int amount) {
+        log("거래 시작: " + getClass().getSimpleName());
+
+        lock.lock();
+        if(balance < amount) {
+            log("[검증 실패]");
+            return false;
+        }
+
+        sleep(1000);
+        balance = balance - amount;
+
+        lock.unlock();
+        log("거래 종료");
+        return true;
+    }
+
+    @Override
+    public synchronized int getBalance() {
+        return balance;
+    }
+}
