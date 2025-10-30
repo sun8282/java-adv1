97c66c9f0135f1de86eb1f9519b8545e13b0e25f
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-10-30 15:55:57 +0900

    add: BoundedMain

--- src/thread/bounded/BoundedMain.java
+++ src/thread/bounded/BoundedMain.java
@@ -1,27 +1,51 @@
 package thread.bounded;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import static util.ThreadUtils.sleep;
 
 public class BoundedMain {
 
     public static void main(String[] args) {
         BoundedQueueV1 queue = new BoundedQueueV1(2);
 
-        prodecerFirst(queue);
-        cunsumerFirst(queue);
+        producerFirst(queue);
+        consumerFirst(queue);
 
     }
 
-    private static void cunsumerFirst(BoundedQueueV1 queue) {
+    private static void consumerFirst(BoundedQueueV1 queue) {
         ArrayList<Thread> threads = new ArrayList<>();
         startProducer(queue, threads);
     }
 
-    private static void startProducer(BoundedQueueV1 queue, ArrayList<Thread> threads) {
+    private static void producerFirst(BoundedQueueV1 queue) {
+        List<Thread> threads = new ArrayList<>();
+        startProducer(queue, threads);
+        startConsumer(queue, threads);
 
     }
 
-    private static void prodecerFirst(BoundedQueueV1 queue) {
-
+    private static void startConsumer(BoundedQueueV1 queue, List<Thread> threads) {
+        System.out.println();
+        for (int i = 1; i < 3; i++) {
+            Thread consumer = new Thread(new ConsumerTask(queue), "consumer" + i);
+            threads.add(consumer);
+            consumer.start();
+            sleep(100);
+        }
     }
+
+    private static void startProducer(BoundedQueueV1 queue, List<Thread> threads) {
+        System.out.println();
+        for (int i = 0; i < 3; i++) {
+            Thread producer = new Thread(new ProducerTask(queue, "data" + i));
+            threads.add(producer);
+            producer.start();
+            sleep(100);
+        }
+    }
+
+
 }
