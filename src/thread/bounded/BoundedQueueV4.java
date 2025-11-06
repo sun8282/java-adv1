e97c0dc0c9a00c2994668a1526020f5e442b6b4f
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-11-07 08:50:15 +0900

    add: BoundedQueueV4

--- src/thread/bounded/BoundedQueueV4.java
+++ src/thread/bounded/BoundedQueueV4.java
@@ -2,11 +2,17 @@ package thread.bounded;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static util.MyLogger.log;
 import static util.ThreadUtils.sleep;
 
 public class BoundedQueueV4 implements BoundedQueue {
+
+    private final Lock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
 
     private final Queue<String> queue = new ArrayDeque<>();
     private final int max;
@@ -16,25 +22,43 @@ public class BoundedQueueV4 implements BoundedQueue {
     }
 
     @Override
-    public synchronized void put(String data) {
-        while(queue.size() == max) {
-            log("큐가 가득참, 생산자 대기");
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+    public void put(String data) {
+        lock.lock();
+        try {
+            while(queue.size() == max) {
+                log("큐가 가득참, 생산자 대기");
+                try {
+                    condition.await();
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
+            queue.offer(data);
+            condition.signal();
+        } finally {
+            lock.unlock();
         }
-        queue.offer(data);
+
+
     }
 
     @Override
-    public synchronized String take() {
-        while (queue.isEmpty()) {
-            log("큐에 데이터 없음, 소비자 대기");
-            sleep(1000);
+    public String take() {
+        lock.lock();
+        try {
+            while (queue.isEmpty()) {
+                condition.await();
+                log("큐에 데이터 없음, 소비자 대기");
+                sleep(1000);
+            }
+            condition.signal();
+            return queue.poll();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
         }
-        return queue.poll();
     }
 
     @Override
