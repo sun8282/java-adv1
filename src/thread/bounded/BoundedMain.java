0a0e499ef105aefe8f16486abaac302837686e72
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-11-06 09:16:58 +0900

    add: BoundedQueueV5

--- src/thread/bounded/BoundedMain.java
+++ src/thread/bounded/BoundedMain.java
@@ -8,7 +8,7 @@ import static util.ThreadUtils.sleep;
 public class BoundedMain {
 
     public static void main(String[] args) {
-        BoundedQueueV3 queue = new BoundedQueueV3(2);
+        BoundedQueueV3 queue = new BoundedQueueV5(2);
 
         producerFirst(queue);
         consumerFirst(queue);
---
+++ src/thread/bounded/BoundedQueueV5.java
@@ -0,0 +1,47 @@
+package thread.bounded;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BoundedQueueV5 implements BoundedQueue {
+
+    private final Queue<String> queue = new ArrayDeque<>();
+    private final int max;
+
+    public BoundedQueueV5(int max) {
+        this.max = max;
+    }
+
+    @Override
+    public synchronized void put(String data) {
+        while(queue.size() == max) {
+            log("큐가 가득참, 생산자 대기");
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        queue.offer(data);
+    }
+
+    @Override
+    public synchronized String take() {
+        while (queue.isEmpty()) {
+            log("큐에 데이터 없음, 소비자 대기");
+            sleep(1000);
+        }
+        return queue.poll();
+    }
+
+    @Override
+    public String toString() {
+        return "BoundedQueueV1{" +
+                "queue=" + queue +
+                ", max=" + max +
+                '}';
+    }
+}
