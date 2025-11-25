c7a0897db492a2489b528c6fc74a63f281cc31cc
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-11-25 15:30:05 +0900

    SimpleListMainV2

--- src/thread/collection/simple/SimpleListMainV2.java
+++ src/thread/collection/simple/SimpleListMainV2.java
@@ -2,13 +2,14 @@ package thread.collection.simple;
 
 import thread.collection.simple.list.BasicList;
 import thread.collection.simple.list.SimpleList;
+import thread.collection.simple.list.SyncList;
 
 import static util.MyLogger.log;
 
 public class SimpleListMainV2 {
 
     public static void main(String[] args) throws InterruptedException {
-        test(new BasicList());
+        test(new SyncList());
     }
 
     private static void test(SimpleList list) throws InterruptedException {
