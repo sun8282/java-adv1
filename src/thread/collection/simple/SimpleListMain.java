aca2e63a8c4e3bd5fb0ff4ed7cedc8122419a66b
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-11-25 14:53:15 +0900

    add: SimpleListMain

---
+++ src/thread/collection/simple/SimpleListMain.java
@@ -0,0 +1,15 @@
+package thread.collection.simple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleListMain {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("A");
+        list.add("B");
+        System.out.println(list);
+    }
+}
