266fcc100f31070a86a23bb4c43e25190ff600d8
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-11-26 09:11:45 +0900

    ExecutorUtils

---
+++ src/thread/executor/ExecutorUtils.java
@@ -0,0 +1,22 @@
+package thread.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static util.MyLogger.log;
+
+public abstract class ExecutorUtils {
+
+    public static void printState(ExecutorService executorService) {
+
+        if (executorService instanceof ThreadPoolExecutor poolExecutor) {
+            int poolSize = poolExecutor.getPoolSize();
+            int activeCount = poolExecutor.getActiveCount();
+            int queued = poolExecutor.getQueue().size();
+            long completedTaskCount = poolExecutor.getCompletedTaskCount();
+            log("[pool=" + poolSize + ", active=" + activeCount + "queuedTasks=" + queued + ", completedTask=" + completedTaskCount);
+        } else {
+            log(executorService);
+        }
+    }
+}
