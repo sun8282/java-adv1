beef6186a718f2eee30a1d074624e8287de1cf3b
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-12-04 17:51:49 +0900

    ExecutorShutdownMain

---
+++ src/thread/executor/ExecutorShutdownMain.java
@@ -0,0 +1,41 @@
+package thread.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static thread.executor.ExecutorUtils.*;
+import static util.MyLogger.log;
+
+public class ExecutorShutdownMain {
+
+    public static void main(String[] args) {
+        ExecutorService es = Executors.newFixedThreadPool(2);
+        es.execute(new RunnableTask("taskA"));
+        es.execute(new RunnableTask("taskB"));
+        es.execute(new RunnableTask("taskC"));
+        es.execute(new RunnableTask("longTask", 100_000));
+
+        printState(es);
+        log("== shutdown 시작");
+        shutdownAndAwatiTermination(es);
+        log("== shutdown 완료");
+        printState(es);
+    }
+
+    private static void shutdownAndAwatiTermination(ExecutorService es) {
+        es.shutdown();
+        try {
+            if(!es.awaitTermination(10, TimeUnit.SECONDS)){
+                log("서비스 정상 종료 실패 -> 강제 종료를 시도");
+                es.shutdownNow();
+
+                if (!es.awaitTermination(10, TimeUnit.SECONDS)){
+                    log("서비스가 종료되지 않았습니다.");
+                }
+            }
+        } catch (InterruptedException e) {
+            es.shutdownNow();
+        }
+    }
+}
