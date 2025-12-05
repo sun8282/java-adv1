bf52b08703d444dc995ceb1bb1929e0ae83ec776
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-12-05 14:23:16 +0900

    PoolSizeMainV3

---
+++ src/thread/executor/poolsize/PoolSizeMainV3.java
@@ -0,0 +1,28 @@
+package thread.executor.poolsize;
+
+import thread.executor.RunnableTask;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static thread.executor.ExecutorUtils.printState;
+import static util.MyLogger.log;
+
+public class PoolSizeMainV3 {
+
+    public static void main(String[] args) {
+        ExecutorService es = Executors.newCachedThreadPool();
+
+        log("pool 생성");
+        printState(es);
+
+        for (int i = 0; i <= 4; i++) {
+                String taskName = "task" + i;
+                es.execute(new RunnableTask(taskName));
+                printState(es, taskName);
+        }
+
+        es.close();
+        log("종료");
+    }
+}
